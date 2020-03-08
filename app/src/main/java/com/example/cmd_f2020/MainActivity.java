2843cdd0d2be781a379bcbdb4ccca13c508e676e
ImBoredRN <43928894+ImBoredRN@users.noreply.github.com>
2020-03-08 08:23:17 -0400

    -saba

--- app/src/main/java/com/example/cmd_f2020/MainActivity.java
+++ app/src/main/java/com/example/cmd_f2020/MainActivity.java
@@ -3,12 +3,66 @@ package com.example.cmd_f2020;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button SundayButtonText = (Button) findViewById(R.id.SundayButtonText);
+        SundayButtonText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Button MondayButtonText = (Button) findViewById(R.id.MondayButtonText);
+        SundayButtonText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Button TuesdayButtonText = (Button) findViewById(R.id.TuesdayButtonText);
+        SundayButtonText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Button WednesdayButtonText = (Button) findViewById(R.id.WednesdayButtonText);
+        SundayButtonText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Button ThursdayButtonText = (Button) findViewById(R.id.ThursdayButtonText);
+        SundayButtonText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        Button FridayButtonText = (Button) findViewById(R.id.FridayButtonText);
+        SundayButtonText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        Button SaturdayButtonText = (Button) findViewById(R.id.SaturdayButtonText);
+        SundayButtonText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }
